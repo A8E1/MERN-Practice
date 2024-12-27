37f46fe78ead9537a94c166ea4c01ba1002182f4
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-27 11:38:16 -0600

    created the model for the collection "Workouts". this is based on the schema we defined. This model is what we interact w/ in order to access the collection.

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
 
+//SCHEMA: defines structure of a type of document in db
+//MODEL: applies schema
 const Schema = mongoose.Schema
 
 const workoutSchema = new Schema({
@@ -20,3 +22,7 @@ const workoutSchema = new Schema({
 
 }, { timestamps: true })
 
+//creates a collection "Workouts", auto-pluralizes. 
+//collection is based on workoutSchema
+//we import this model to interact w the collection "Workouts"
+module.exports = mongoose.model('Workout', workoutSchema)
