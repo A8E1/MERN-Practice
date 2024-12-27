776761607a4ca49fca262a30cd1764430262b442
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-27 11:45:31 -0600

    POST request to create a new workout, destructured creation of 3 variables from body, which is parsed by middleware in server.js. changed the post handler to async, used the await keyword to create a new document using the workoutModel from models folder. Send back document and status code 200 if success, sends back mongo error message if fails.

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 const router = express.Router()
 
 router.get('/', (req,res)=>{
@@ -9,8 +10,20 @@ router.get('/:id', (req,res)=>{
     res.json({mssg: "get ONE workouts"})
 })
 
-router.post('/', (req, res) => {
-    res.json({mssg: "create workout"})
+//POST request to create a new workout
+//destructured creation of 3 variables from body, which is parsed by middleware in 
+//server.js
+
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+
+    try{
+        const workout = await Workout.create({title, load, reps})
+
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 
 router.patch('/:id', (req, res)=>{
