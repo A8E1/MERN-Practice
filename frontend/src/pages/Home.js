0e8b644fd7fb0905b735fbdef584373d9037579d
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-28 11:30:14 -0600

    added the workouts state useEffect used to fetch all workout data from the backend, set that data into the state using ok property from fetch response
    then we added the div workouts, and made a conditional check. 1st sees if workouts is not null aka has been defined, then uses map function to display each workout's title with the key being the mongoose ID property each document had
    
    notes for myself:
    I dont fully understand states, and i dont understand how useEffect changes state. I also don't understand what the role of the empty dependecy array is. I also don't get how in the workouts div, how some words are wrapped in () and {}. I don't fully understand how the conditional check works either.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
-//we wil
+//we will display all workouts on the home page
+
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    //workout is the state, setWorkouts changes the state, and workouts = null
+    const [workouts, setWorkouts] = useState(null)
+
+
+    //will only fire once, when component first renders bc of '[]' dependency arr
+    //fetches backend data
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('http://localhost:3000/api/workouts')
+            //parses json, creates array of workout objects
+            const json = await response.json()
+            
+            if(response.ok){
+                setWorkouts(json)
+            }
+        }
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
